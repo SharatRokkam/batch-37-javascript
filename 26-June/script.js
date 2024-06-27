030e1bdc99b44589063b0836573e98cb25e2139e
SharatRokkam <rokkamsharath1@gmail.com>
2024-06-27 13:19:28 +0530

    arrow fun, scope and Execution context & callstack

--- 26-June/script.js
+++ 26-June/script.js
@@ -81,36 +81,36 @@
 // 3. Arrow Functions
 // 4. Immediately Invoked function expression
 
-// 1. function expression is oldest way of defining the function
+// 1. function declaration is oldest way of defining the function
 
 // Every function will have function name , parameter, arguments and return
 
 //creation a function
 
 // a,b are parameter which is being passed to function()
-// function sum(a, b, c) {
-//     console.log(a + b);
-//   return a + b + c;
-// }
+function sum(a, b, c) {
+    console.log(a + b);
+  return a + b + c;
+}
 
 //invocation or calling of a function
-// console.log(sum(5, 10, 12));
+console.log(sum(5, 10, 12));
 // console.log(sum(10, 10, 122));
 // console.log(sum(13, 10, 12));
 
-//Function declaration
+//Function expression
 
-// const square = function (a) {
-//   console.log(a * a);
-// };
+let square = function (a) {
+  console.log(a * a);
+};
 
-// square(5);
+square(5);
 
 // IIFE - needs to get executed just after it is defined, it is used execute something without interuppting the global scope as it create its own scope, it does not pollute the variables and function in global scope.
 
-(function (name) {
-  console.log("Hello " + name + "!");
-})("Dave");
+// (function (name) {
+//   console.log("Hello " + name + "!");
+// })("Dave");
 
 // Questions to solve
 // 1. Iterate over the properties of the following object and print only the properties with numeric values
@@ -120,5 +120,5 @@
 // const data = { a: 10, b: "hello", c: 20, d: "World" };
 
 //3. Iterate over the array and print elements that are greater than 10
-// const array = [4, 13, 44, 3, 5, 55];
+//const array = [4, 13, 44, 3, 5, 55];
 
