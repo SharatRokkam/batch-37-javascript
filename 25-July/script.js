66ea674c7f6cfc6893ffa1180008326fa301ee8a
SharatRokkam <rokkamsharath1@gmail.com>
2024-07-25 15:21:46 +0530

    jquery

---
+++ 25-July/script.js
@@ -0,0 +1,55 @@
+//jQuery is Javascript Library that came in the year 2006. It was created by John Resig to ease Js programming. After learning the concepts of jQuery. It will be easier for a developer to work with javascript. It is a "write less, do more" JS library...
+
+// jQuery eases Js code. The same code you run in js with many lines can be easily worked with jQuery and lessened to a single line of code..
+
+// features
+// 1. Selectors 2. Animation 3. CSS Manipulation 4. AJAX 5. Event Methods 6. Plugins (slideshow, progress bar, split pages)
+
+// Basic Syntax
+// The basic syntax is: $(selector).action()
+
+// $ is used to define/access jQuery.
+// (selector) is used to "query (or find)" HTML elements.
+
+// action() is used to perform some action on the element(s).
+
+// CDN : Content Delivery Network
+
+// $(document).ready(function () {
+//   $("#hide").dblclick(function () {
+//     $("p").hide();
+//   })
+//     $("#show").dblclick(function () {
+//       $("p").show();
+//     });
+// });
+
+// $(document).ready(function () {
+//   $("#toggle").click(function () {
+//     $("p").toggle();
+//   });
+// });
+
+// content manipulation
+// html(), text(), val(),
+//attribute manipulation
+// attr(), removeAttr()
+// css manipulation
+// css(), addClass(), removeClass();
+
+// let content = $("#element").html();
+// let content = $("#element").text();
+
+// console.log(content);
+// $("#element").html("Text will be changed");
+// $("#element").text("Text will be changed second time");
+
+// $(document).ready(function () {
+//   $("#element").css({
+//     color: "red",
+//     border: "1px solid black",
+//     backgroundColor: "yellow",
+//     textAlign: "center",
+//   });
+// });
+
